2620f51528cf349ceafdb8fed407ef79211df865
leavez <gaojiji@gmail.com>
2018-03-19 16:17:17 +0800

    remove skipping integrate_targets

--- lib/cocoapods-prebuild-framework/rome/pre_install.rb
+++ lib/cocoapods-prebuild-framework/rome/pre_install.rb
@@ -1,6 +1,4 @@
 Pod::HooksManager.register('cocoapods-prebuild-framework', :pre_install) do |installer_context|
   podfile = installer_context.podfile
   podfile.use_frameworks!
-  podfile.install!('cocoapods',
-    podfile.installation_method.last.merge(:integrate_targets => false))
 end
