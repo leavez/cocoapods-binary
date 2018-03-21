453d98af061b555abe0fa950b49620ef3f05aa51
leavez <gaojiji@gmail.com>
2018-03-21 15:09:37 +0800

    hook the `pod` DSL to make it only valid for 'prebuild => true'

---
+++ lib/cocoapods-prebuild-framework/framework_target_generator.rb
@@ -0,0 +1,48 @@
+
+# hook the `pod` DSL to make it only valid for 'prebuild => true'
+module Pod
+    class Podfile
+        module DSL
+            
+            @@enable_prebuild_patch = false
+            def self.enable_prebuild_patch(value)
+                @@enable_prebuild_patch = value
+            end
+
+            # --- patch ---
+            old_method = instance_method(:pod)
+
+            define_method(:pod) do |name, *args|
+                if !@@enable_prebuild_patch
+                    old_method.bind(self).(name, *args)
+                    return
+                end
+
+                options = args.last
+                return unless options.is_a?(Hash)
+                prebuild = options[:prebuild_framework]
+                if prebuild
+                    old_method.bind(self).(name, *args)
+                end
+            end
+         end
+    end
+end
+
+
+module Pod
+    class Command
+        class Install
+            
+            # --- patch ---
+            old_method = instance_method(:run)
+            
+            define_method(:run) do 
+                Pod::Podfile::DSL.enable_prebuild_patch true
+                old_method.bind(self).()
+            end
+
+          
+        end
+    end
+end
--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,3 +1,4 @@
+require 'cocoapods-prebuild-framework/podfile_options'
+require 'cocoapods-prebuild-framework/framework_target_generator'
 require 'cocoapods-prebuild-framework/rome/post_install'
 require 'cocoapods-prebuild-framework/rome/pre_install'
-require 'cocoapods-prebuild-framework/podfile_options'
