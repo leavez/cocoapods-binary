dd19f46db72d6b8a4c3ca0012590472d32cee21c
leavez <gaojiji@gmail.com>
2018-04-08 23:43:58 +0800

    fix reading symlink in embeded frameworks script

--- lib/cocoapods-binary/Integration.rb
+++ lib/cocoapods-binary/Integration.rb
@@ -142,3 +142,38 @@ module Pod
     end
 end
 
+# A fix in embeded frameworks script.
+#
+# The framework file in pod target folder is a symblink. The EmbedFrameworksScript use `readlink`
+# to read the read path. As the symlink is a relative symlink, readlink cannot handle it well. So 
+# we override the `readlink` to a fixed version.
+#
+module Pod
+    module Generator
+        class EmbedFrameworksScript
+
+            old_method = instance_method(:script)
+            define_method(:script) do
+
+                script = old_method.bind(self).()
+                patch = <<-SH.strip_heredoc
+                    #!/bin/sh
+                
+                    # ---- this is added by cocoapods-binary ---
+                    # Readlink cannot handle relative symlink well, so we override it to a new one
+                    # If the path isn't an absolute path, we add a realtive prefix.
+                    readlink () {
+                        path=`/usr/bin/readlink $1`;
+                        if [ $(echo "$path" | cut -c 1-1) = '/' ]; then
+                            echo $path;
+                        else
+                            echo "`dirname $1`/$path";
+                        fi
+                    }
+                    # --- 
+                SH
+                patch + script
+            end
+        end
+    end
+end
