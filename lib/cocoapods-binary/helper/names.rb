4c2a3ad06b6c19b3fba76706d179fbd261063638
leavez <gaojiji@gmail.com>
2018-07-07 03:53:32 +0800

    add comments for names

--- lib/cocoapods-binary/helper/names.rb
+++ lib/cocoapods-binary/helper/names.rb
@@ -1,3 +1,15 @@
+#
+# There are many kinds of name in cocoapods. Two main names are widely used in this plugin.
+# - root_spec.name (spec.root_name, targe.pod_name):
+#   the name we use in podfile. the concept.
+# 
+# - target.name:
+#   the name of the final target in xcode project. the final real thing.
+#
+# One pod may have multiple targets in xcode project, due to one pod can be used in mutiple 
+# platform simultaneously. So one `root_spec.name` may have multiple coresponding `target.name`s.
+# Therefore, map a spec to/from targets is a little complecated. It's one to many.
+#
 module Pod
 
     def self.possible_target_names_from_pod_name(root_pod_name) 
