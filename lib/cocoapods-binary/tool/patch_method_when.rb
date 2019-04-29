c2e6276df0780fca2c6ee5c20d30320e6fb02e18
Leavez <gaojiji@gmail.com>
2019-04-29 16:54:48 +0800

    Update patch_method_when.rb

--- lib/cocoapods-binary/tool/patch_method_when.rb
+++ lib/cocoapods-binary/tool/patch_method_when.rb
@@ -1,4 +1,5 @@
 require_relative 'patch_method'
+require_relative 'assert'
 
 # Only execute the patched content when condition is true,
 # otherwise execute the original method.
@@ -6,6 +7,7 @@ require_relative 'patch_method'
 # @param [Proc return bool] condition
 # @see patch_method
 def patch_method_when(method_name, condition, &content)
+    assert_type condition, Proc
     patch_method(method_name) do |old_method, args|
         if condition.call
             instance_exec old_method, args, &content
@@ -17,6 +19,7 @@ end
 
 # @see patch_method_after
 def patch_method_after_when(method_name, condition, &content)
+    assert_type condition, Proc
     patch_method(method_name) do |old_method, args|
         if condition.call
             old_result = old_method.(*args)
@@ -30,6 +33,7 @@ end
 
 # @see patch_method_before
 def patch_method_before_when(method_name, condition, &content)
+    assert_type condition, Proc
     patch_method(method_name) do |old_method, args|
         if condition.call
             instance_exec *args, &content
