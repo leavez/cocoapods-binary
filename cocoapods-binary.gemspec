d5d97f7eb940f80f405dc54d8300289ef3a99df6
yrainik <yrainik@godaddy.com>
2019-05-10 22:21:35 -0700

    Add s3 integration

--- cocoapods-binary.gemspec
+++ cocoapods-binary.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "cocoapods", ">= 1.5.0", "< 2.0"
   spec.add_dependency "fourflusher", "~> 2.0"
   spec.add_dependency "xcpretty", "~> 0.3.0"
+  spec.add_dependency "aws-sdk-s3", "~> 1"
 
   spec.add_development_dependency 'bundler', '~> 1.3'
   spec.add_development_dependency 'rake'
--- lib/cocoapods-binary/helper/shared_cache.rb
+++ lib/cocoapods-binary/helper/shared_cache.rb
@@ -1,5 +1,7 @@
+require 'aws-sdk-s3'
 require 'digest'
 require_relative '../tool/tool'
+require 'zip'
 
 module Pod
     class Prebuild
@@ -12,27 +14,89 @@ module Pod
             # @return [Boolean]
             def self.has?(target, options)
                 if Podfile::DSL.shared_cache_enabled
-                    framework_cache_path_for(target, options).exist?
+                    path = framework_cache_path_for(target, options, true)
+                    result = path.exist?
+                    if not result and Podfile::DSL.shared_s3_cache_enabled
+                        s3_cache_path = framework_cache_path_for(target, options, false)
+                        s3_cache_path = Podfile::DSL.s3_options[:prefix] + s3_cache_path  unless Podfile::DSL.s3_options[:prefix].nil?
+                        s3 = Aws::S3::Resource.new(create_s3_options)
+                        if s3.bucket(Podfile::DSL.s3_options[:bucket]).object("#{s3_cache_path}").exists?
+                            Dir.mktmpdir {|dir|
+                                s3.bucket(Podfile::DSL.s3_options[:bucket]).object("#{s3_cache_path}").get(response_target: "#{dir}/framework.zip")
+                                unzip("#{dir}/framework.zip", path)
+                                return true
+                            }
+                        end
+                    end
+                    result
                 else
                     false
                 end
             end
 
-            # Copies input_path to target's cache
+            # @return [{}] AWS connection options
+            def self.create_s3_options
+                options = {}
+                creds = Aws::Credentials.new(Podfile::DSL.s3_options[:login], Podfile::DSL.s3_options[:password]) unless Podfile::DSL.s3_options[:login].nil? and Podfile::DSL.s3_options[:password].nil?
+                options[:credentials] = creds unless creds.nil?
+                options[:region] = Podfile::DSL.s3_options[:region] unless Podfile::DSL.s3_options[:region].nil?
+                options[:endpoint] = Podfile::DSL.s3_options[:endpoint] unless Podfile::DSL.s3_options[:endpoint].nil?
+
+                options
+            end
+
+            def self.zip(dir, zip_dir)
+                Zip::File.open(zip_dir, Zip::File::CREATE)do |zipfile|
+                    Find.find(dir) do |path|
+                        Find.prune if File.basename(path)[0] == ?.
+                        dest = /#{dir}\/(\w.*)/.match(path)
+                        # Skip files if they exists
+                        begin
+                            zipfile.add(dest[1],path) if dest
+                        rescue Zip::ZipEntryExistsError
+                        end
+                    end
+                end
+            end
+
+            def self.unzip(zip, unzip_dir, remove_after = false)
+                Zip::File.open(zip) do |zip_file|
+                    zip_file.each do |f|
+                        f_path=File.join(unzip_dir, f.name)
+                        FileUtils.mkdir_p(File.dirname(f_path))
+                        zip_file.extract(f, f_path) unless File.exist?(f_path)
+                    end
+                end
+                FileUtils.rm(zip) if remove_after
+            end
+
+            # Copies input_path to target's cache and save to s3 if applicable
             def self.cache(target, input_path, options)
                 if not Podfile::DSL.shared_cache_enabled
                     return
                 end
-                cache_path = framework_cache_path_for(target, options)
+                cache_path = framework_cache_path_for(target, options, true)
                 cache_path.mkpath unless cache_path.exist?
                 FileUtils.cp_r "#{input_path}/.", cache_path
+                if Podfile::DSL.shared_s3_cache_enabled
+                    s3_cache_path = framework_cache_path_for(target, options, false)
+                    s3 = Aws::S3::Resource.new(create_s3_options)
+                    Dir.mktmpdir {|dir|
+                        zip(cache_path, "#{dir}/framework.zip")
+                        s3.bucket(Podfile::DSL.s3_options[:bucket]).object("#{Podfile::DSL.s3_options[:prefix]}/#{s3_cache_path}").upload_file("#{dir}/framework.zip")
+                    }
+                end
             end
 
             # Path of the target's cache
             #
             # @return [Pathname]
             def self.framework_cache_path_for(target, options)
-                framework_cache_path = cache_root + xcode_version
+                framework_cache_path = Pathname.new('')
+                if include_cache_root
+                    framework_cache_path = cache_root
+                end
+                framework_cache_path = framework_cache_path + xcode_version
                 framework_cache_path = framework_cache_path + target.name
                 framework_cache_path = framework_cache_path + target.version
                 options_with_platform = options + [target.platform.name]
