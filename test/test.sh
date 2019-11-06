9a6d214b1f046466aeb62f571b70ec1f3313be5c
Kyle Fleming <kyle@kylefleming.net>
2019-11-06 16:47:50 +0900

    Add DerivedData delete step to tests

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -ex
 
+clean() {
+    rm -rf ~/Library/Developer/Xcode/DerivedData/*/
+}
+
 build() {
     xcodebuild -workspace Binary.xcworkspace -scheme Binary ONLY_ACTIVE_ARCH=YES  CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO -quiet || exit 1 
 }	
@@ -10,6 +14,7 @@ rm -rf Pods
 cases=("initial" "addSwiftPod" "revertToSourceCode" "addDifferentNamePod" "addSubPod" "deleteAPod" "addVendoredLibPod" "universalFlag" "multiplePlatforms" "multiplePlatformsWithALLFlag" "oldPodVersion" "upgradePodVersion" "originalPodfileAndLockfileVersion" "upgradePodfileAndLockfileVersion" "originalLockfileVersion" "upgradeLockfileVersion")
 for action in ${cases[@]}; do
     python change_podfile.py ${action}
+    clean
     bundle exec pod install
     build
 done
